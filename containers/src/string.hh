83702a9eea2bcc8e26cb5922d87d14711b308313
Luke Hodkinson <furious.luke@gmail.com>
2012-08-09 17:30:27 +1000

    Adding a little wrapper meaning I don't have to continually write std::string.

---
+++ containers/src/string.hh
@@ -0,0 +1,12 @@
+#ifndef libhpc_containers_string_hh
+#define libhpc_containers_string_hh
+
+#include <string>
+
+namespace hpc {
+
+   // TODO: Would like to inherit, but shitty gcc can't inherit constructors.
+   typedef std::string string;
+}
+
+#endif
