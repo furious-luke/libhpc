68d7ce804d24980e3c45905db45cb9365dffbe31
Luke Hodkinson <furious.luke@gmail.com>
2013-09-13 20:28:41 +1000

    Adding a class to help construct HDF5 derived datatypes.

---
+++ src/h5/derive.hh
@@ -0,0 +1,63 @@
+// Copyright 2012 Luke Hodkinson
+
+// This file is part of libhpc.
+// 
+// libhpc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// libhpc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with libhpc.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef libhpc_h5_derive_hh
+#define libhpc_h5_derive_hh
+
+#include "datatype.hh"
+
+namespace hpc {
+   namespace h5 {
+
+      class derive
+      {
+      public:
+
+	typedef std::tuple< h5::datatype const*,
+			    hsize_t,
+			    h5::datatype const*,
+			    std::string > entry_type;
+
+      public:
+
+	void
+	add( const h5::datatype& mem_type,
+	     hsize_t mem_offs,
+	     const h5::datatype& file_type,
+	     const std::string& desc = "" );
+
+	 void
+	 commit( h5::datatype& mem_type,
+		 h5::datatype& file_type );
+
+      protected:
+
+	 hsize_t
+	 _calc_mem_size();
+
+	 hsize_t
+	 _calc_file_size();
+
+      protected:
+
+	 std::list<entry_type> _cache;
+      };
+
+   }
+}
+
+#endif
---
+++ tests/h5/derive_suite.cc
@@ -0,0 +1,49 @@
+// Copyright 2012 Luke Hodkinson
+
+// This file is part of libhpc.
+// 
+// libhpc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// libhpc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with libhpc.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <libhpc/debug/unit_test_main.hh>
+#include "libhpc/h5/derive.hh"
+
+using namespace hpc;
+using namespace hpc::test;
+
+namespace {
+
+   struct test_struct
+   {
+      int an_int;
+      byte a_byte;
+      float a_float;
+   };
+
+   test_case<> ANON(
+      "/h5/derive",
+      "",
+      []()
+      {
+	 h5::derive der;
+	 der.add( h5::datatype::native_int, HOFFSET( test_struct, an_int ), h5::datatype::std_i32be, "An integer!" );
+	 der.add( h5::datatype::native_char, HOFFSET( test_struct, a_byte ), h5::datatype::native_char, "A byte!" );
+	 der.add( h5::datatype::native_float, HOFFSET( test_struct, a_float ), h5::datatype::native_float, "A float!" );
+	 h5::datatype mem_type, file_type;
+	 der.commit( mem_type, file_type );
+	 TEST( mem_type.size() == 9 );
+	 TEST( file_type.size() == 9 );
+      }
+      );
+
+}
