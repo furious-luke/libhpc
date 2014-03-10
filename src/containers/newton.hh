100342f6c09c03ad58e2da74f35d572f2c7f2905
Luke Hodkinson <furious.luke@gmail.com>
2014-03-11 09:38:56 +1100

    Messing around with the Newton root finder to try to make it more stable.

--- src/containers/newton.hh
+++ src/containers/newton.hh
@@ -20,6 +20,8 @@
 
 #include <boost/optional/optional.hpp>
 #include "libhpc/debug/assert.hh"
+#include "libhpc/debug/except.hh"
+#include "libhpc/logging/logging.hh"
 #include "num.hh"
 
 using boost::optional;
@@ -27,7 +29,8 @@ using boost::optional;
 namespace hpc {
    namespace num {
 
-      static const double default_newton_tolerance = 1e-8;
+      static constexpr double default_newton_tolerance = 1e-8;
+      static constexpr unsigned default_newton_max_its = 60;
 
       template< class Function,
 		class T >
@@ -37,22 +40,32 @@ namespace hpc {
               T x2,
               optional<T> initial = optional<T>(),
               optional<T&> deriv = optional<T&>(),
-              T tolerance = default_newton_tolerance )
+              T tolerance = default_newton_tolerance,
+	      unsigned max_its = default_newton_max_its,
+	      T omega = 1.0 )
       {
-	 static const unsigned max_its = 60;
+	 LOGBLOCKT_TAG( "newton", "Newton-Raphson solve:" );
 
          // Were we given an initial value? If not, set it to the midpoint.
          T x = initial ? *initial : 0.5*(x2 + x1);
+	 LOGTLN( "Starting with x: ", x );
 
          // Only loop up until or maximum.
          T f, df, delta;
          int ii;
          for( ii = 0; ii < max_its; ++ii )
          {
+	    LOGBLOCKT( "Iteration: ", ii );
+
             f = func( x );
             df = func.derivative( x, f );
             delta = f/df;
-            x -= delta;
+	    LOGTLN( "f(", x, ") = ", f );
+	    LOGTLN( "df/dx = ", df );
+	    LOGTLN( "delta x = ", delta );
+	    LOGTLN( "old x = ", x );
+            x = (1.0 - omega)*x + omega*(x - delta);
+	    LOGTLN( "new x = ", x );
 
             // Values must remain bracketed.
             ASSERT( (x1 - x)*(x - x2) >= 0.0,
@@ -64,13 +77,76 @@ namespace hpc {
          }
 
          // Break if we exceeded maximum its.
-         ASSERT( ii < max_its );
+         EXCEPT( ii < max_its );
 
          // Store function derivative?
          if( deriv )
             *deriv = df;
 
          return x;
+      }
+
+      template< class Function,
+		class T >
+      bool
+      newton_test( Function& func,
+		   T x1,
+		   T x2,
+		   T& result,
+		   optional<T> initial = optional<T>(),
+		   optional<T&> deriv = optional<T&>(),
+		   T tolerance = default_newton_tolerance,
+		   unsigned max_its = default_newton_max_its,
+		   T omega = 1.0 )
+      {
+	 LOGBLOCKT_TAG( "newton", "Newton-Raphson solve:" );
+
+         // Were we given an initial value? If not, set it to the midpoint.
+         T x = initial ? *initial : 0.5*(x2 + x1);
+	 T init = x;
+	 LOGTLN( "Starting with x: ", x );
+
+         // Only loop up until or maximum.
+         T f, df, delta;
+         int ii;
+         for( ii = 0; ii < max_its; ++ii )
+         {
+	    LOGBLOCKT( "Iteration: ", ii );
+
+            f = func( x );
+            df = func.derivative( x, f );
+            delta = f/df;
+	    LOGTLN( "f(", x, ") = ", f );
+	    LOGTLN( "df/dx = ", df );
+	    LOGTLN( "delta x = ", delta );
+	    LOGTLN( "old x = ", x );
+            x = (1.0 - omega)*x + omega*(x - delta);
+	    LOGTLN( "new x = ", x );
+
+            // Values must remain bracketed.
+            if( (x1 - x)*(x - x2) < 0.0 )
+	    {
+	       result = init;
+	       return false;
+	    }
+
+            // Check for convergence.
+            if( fabs( delta ) < tolerance )
+               break;
+         }
+
+	 // Store result.
+	 result = x;
+
+         // Store function derivative?
+         if( deriv )
+            *deriv = df;
+
+         // Break if we exceeded maximum its.
+         if( ii == max_its )
+	    return false;
+
+         return true;
       }
 
    }
