ef30a7ecb63f4fe61573d355dab0387ca136b9cd
Luke Hodkinson <furious.luke@gmail.com>
2014-06-01 08:30:09 +1000

    Or exceptions... (see previous commit)

--- src/debug/except.hh
+++ src/debug/except.hh
@@ -15,10 +15,10 @@
 // You should have received a copy of the GNU General Public License
 // along with libhpc.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef libhpc_debug_except_hh
-#define libhpc_debug_except_hh
+#ifndef hpc_debug_except_hh
+#define hpc_debug_except_hh
 
-#ifndef NEXCEPT
+#if !defined(NEXCEPT) && !defined(__CUDA_ARCH__)
 
 #include "assert.hh"
 
